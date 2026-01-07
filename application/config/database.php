5c5e45e431022b3b85042851cfd453b4bb608465
AnsHPaTeL2927 <116540143+AnsHPaTeL2927@users.noreply.github.com>
2026-01-07 22:53:04 +0530

    add db name in project

--- application/config/database.php
+++ application/config/database.php
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'zuku',
+	'database' => 'itaca_new',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -93,6 +93,5 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE,
-	'port' => 1829
 );
 
